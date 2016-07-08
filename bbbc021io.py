3d02136ac50fd35ba0d436e60fed21ad10937fba
Don86 <Don86@users.noreply.github.com>
2016-07-08 16:14:42 +1000

    Add files via upload

--- bbbc021io.py
+++ bbbc021io.py
@@ -22,17 +22,17 @@ def get_main_list(path):
     for subfolder in subfolders:
         if "Week" in subfolder:
             subfolders2.append(subfolder)
-            
+
     return subfolders2
 
 
 def get_platenum(fn):
     """Extracts the plate number from the given file name
-    
+
     Params
     ------
     fn: str. Filename
-    
+
     Returns
     -------
     platenum: str
@@ -47,15 +47,42 @@ def get_platenum(fn):
     return platenum
 
 
-def get_all_data(path, n_patches, patch_len, db, verbose=1):
-    """main proc to do everything. May not be required."""
-    return
+def get_plate_well(fn):
+    """Extracts the plate number from the given file name
+
+    Params
+    ------
+    fn: str. File name.
+
+    Returns
+    -------
+    platewell: str. Plate-number/well-number coordinates.
+
+    """
+    pattern = re.compile(r'_[\d]*')
+    pnum = re.findall(pattern, fn)
+    if len(pnum) > 1:
+        print("Warning: %s matches for plate number found" % len(pnum))
+        print("Default behaviour: use first match")
+    platenum = pnum[0].strip('_')
+
+    pattern2 = re.compile(r'_[A-z][0-9][0-9]_')
+    wn = re.findall(pattern2, fn)
+    if len(wn) > 1:
+        print("Warning: %s matches for plate number found" % len(wn))
+        print("Default behaviour: use first match")
+    wellnum = wn[0].strip('_')
+
+    platewell = platenum + '-' + wellnum
+
+    return platewell
+
 
 def get_subfolder_patch_data(subpath, n_patches, patch_len, db, verbose=1):
     """At the subfolder level, get the platenum.
-    At image level, get well numbers and image data from all files in the given 
+    At image level, get well numbers and image data from all files in the given
     subfolder path
-    
+
     Params
     ------
     subpath: str. Path of subfolder
@@ -67,7 +94,7 @@ def get_subfolder_patch_data(subpath, n_patches, patch_len, db, verbose=1):
         0: All comments suppressed
         1: Only prints comments at subfolder level
         2: Prints all comments
-    
+
     Return
     ------
     subfolder_data_arr: array. each row = [patches, plate num, well num]
@@ -77,7 +104,7 @@ def get_subfolder_patch_data(subpath, n_patches, patch_len, db, verbose=1):
     sf_list = os.listdir(subpath)
     if verbose > 0:
         print("%s images found in subfolder" % len(sf_list))
-    
+
     sf_data_list = []
     idx = 1
     for fn in sf_list:
@@ -101,34 +128,34 @@ def get_subfolder_patch_data(subpath, n_patches, patch_len, db, verbose=1):
             if verbose > 1:
                 print("Warning: %s results found for %s, returning first row." % (len(result), plate_well))
                 print("%s. %s: %s-%s, moa: %s" % (idx, plate_well, c1, c2, moa))
-        
+
         idx +=1
-    
+
     return sf_data_list
-    
+
 
 def get_image_patches_and_wellnum(fn, subpath, n_patches, patch_len):
     """Operates at image-level.
     Get the image data itself, and the well num from the filename itself
     Uses some patch extraction method, instead of imread wholesale
     so that not the entire image is extracted
-    
+
     Returns
     -------
     patches: array. Patches of the image. Shape (n_patches, patch_len, patch_len, ch)
     wellnum: str. Well number as extracted from the filename itself
     """
-    
+
     #Get the image data
     fn_path = subpath + "/" + fn
     img = img_as_float(io.imread(fn_path)[:,:,:3])
     im_height, im_width, ch = img.shape
-    
+
     #for the moment, let's just generate new patch coordinates for each image
     #We'll think about using the same patch coords across all images later
     coords = pex.generate_patch_coords(n_patches, patch_len, im_width, im_height)
     patches = pex.extract_patches_3d(img, coords, patch_len)
-    
+
     #Get well numbers, which typically appear between '_', e.g.'_B07_'
     #Or at the front of the file name, e.g. 'B06_'
     pattern = re.compile(r'_[A-z][0-9][0-9]_|^[A-z][0-9][0-9]_')
@@ -137,34 +164,33 @@ def get_image_patches_and_wellnum(fn, subpath, n_patches, patch_len):
         print("Warning: multiple matches for well num found")
         print("Default behaviour: use first match")
     wellnum = wn[0].strip('_')
-    
+
     return patches, wellnum
 
 
 def search_labels(platewell, db1):
-    """Search for the compound-concentration labels in a db, 
+    """Search for the compound-concentration labels in a db,
     given the plate-well coordinates
-    
+
     Params
     ------
     platewell: str, format 'BBBC021-<plate no.>-<well no.>
-    db1: a pandas dataframe, 
+    db1: a pandas dataframe,
         with columns 'plate-well', 'compound', 'concentration', 'moa'
-    
+
     Returns
     -------
     results: array of search results. Each row = 1 result:
         (plate_well, compound, concentration, moa)
     """
-    
+
     query = [platewell]
     result = db1.loc[db1['plate-well'].isin(query)]
     result = np.array(result)
-    
+
     return result
-    
 
 
 if __name__ == '__main__':
-    import mnistio
+    import patch_extraction as pex
     print('accuracy: %.3f' % np.mean((yts_pred == yts).astype(float)))
